4e0ca4f684761896d411e87c25dbae1b313f4b9c
Ashish Kumar Maurya <ashishkumarmaurya607@gmail.com>
2023-09-05 13:07:52 +0530

    "initial commit"

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,40 @@
+import "./share.css"
+import PermMediaIcon from '@mui/icons-material/PermMedia';
+import LabelIcon from '@mui/icons-material/Label';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+
+export default function Share() {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+            <img className="shareProfileImg" src="/assets/person/1.jpeg" alt="" />
+            <input placeholder="What's in your mind shubham?" className="shareInput" />
+        </div>
+        <hr className="shareHr" />
+        <div className="shareBottom">
+            <div className="shareOptions">
+                <div className="shareOption">
+                  <PermMediaIcon htmlColor="tomato" className="shareIcon"/>
+                  <span className="shearOptionText">Photo or Video</span>                   
+                </div>
+                <div className="shareOption">
+                  <LabelIcon htmlColor="blue" className="shareIcon"/>
+                  <span className="shearOptionText">Tag</span>                   
+                </div>
+                <div className="shareOption">
+                  <LocationOnIcon htmlColor="green" className="shareIcon"/>
+                  <span className="shearOptionText">Location</span>                   
+                </div>
+                <div className="shareOption">
+                  <EmojiEmotionsIcon htmlColor="goldenrod" className="shareIcon"/>
+                  <span className="shearOptionText">Feelings</span>                   
+                </div>
+            </div>
+            <button className="shareButton">Share</button>
+        </div>
+      </div>
+    </div>
+  )
+}
